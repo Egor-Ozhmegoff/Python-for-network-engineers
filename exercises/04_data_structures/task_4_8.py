7de9fc5ff1a378522ecbdaa3e727dedd2a8c81c7
Egor-Ozhmegoff <banezmesm@gmail.com>
2020-11-04 23:24:06 +0300

    Сделано задание 4.8

--- exercises/04_data_structures/task_4_8.py
+++ exercises/04_data_structures/task_4_8.py
@@ -20,3 +20,10 @@
 """
 
 ip = "192.168.3.1"
+ip = list(ip.split('.'))
+ip_template = '''
+IP address:
+{0:<8}  {1:<8}  {2:<8}  {3:<8}
+{0:08b}  {1:08b}  {2:08b}  {3:08b}
+'''
+print(ip_template.format(int(ip[0]),int(ip[1]),int(ip[2]),int(ip[3])))
