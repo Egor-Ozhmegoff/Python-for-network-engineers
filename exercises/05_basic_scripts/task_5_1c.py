78fc2b9bddc35042676df898973659f9c11a8514
Egor-Ozhmegoff <banezmesm@gmail.com>
2020-11-05 17:15:53 +0300

    Сделано задание 5.1c

--- exercises/05_basic_scripts/task_5_1c.py
+++ exercises/05_basic_scripts/task_5_1c.py
@@ -49,3 +49,8 @@ london_co = {
         "routing": True,
     },
 }
+device = input("Введите имя устройства: ")
+params = ", ".join(london_co[device].keys())
+parameter = input(f"Введите имя параметра ({params}): ")
+
+print(london_co[device].get(parameter,'Такого параметра нет'))
