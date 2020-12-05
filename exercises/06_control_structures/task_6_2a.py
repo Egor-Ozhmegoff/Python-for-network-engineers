7f52a6593d7a6404bada4dcae06484cb7bb0b584
Egor-Ozhmegoff <banezmesm@gmail.com>
2020-12-05 20:12:06 +0300

    Сделано задание 6.2

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -17,3 +17,29 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+ip = input("Введите ip адрес в формате 10.0.1.1:")
+ip_list = list(ip.split("."))
+correct_ip = False
+
+if len(ip_list) != 4:
+    correct_ip = True
+else:
+    for oct in ip_list :
+        if not (oct.isdigit() and int(oct) in range(256)) :
+	        correct_ip = True
+        	break
+if correct_ip :
+    print("Неправильный IP-адрес")
+else :
+    oct1 = int(ip_list[0])
+
+    if oct1 >= 1 and oct1 <= 223 :
+        print("unicast")
+    elif oct1 >= 224 and oct1 <= 239 :
+        print("multicast")
+    elif ip == '255.255.255.255' :
+        print("broadcast")
+    elif ip == '0.0.0.0' :
+        print("unassigned")
+    else :
+        print("unused")
