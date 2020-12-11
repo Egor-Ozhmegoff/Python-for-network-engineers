1d20313e7631287c5ff15bde437afcda03ee9221
Egor-Ozhmegoff <banezmesm@gmail.com>
2020-12-11 12:37:33 +0300

    Сделано задание 7.2b

--- exercises/07_files/task_7_2b.py
+++ exercises/07_files/task_7_2b.py
@@ -14,3 +14,14 @@
 """
 
 ignore = ["duplex", "alias", "Current configuration"]
+ignore = ["duplex", "alias", "Current configuration"]
+with open('config_sw1.txt','r') as f , open('config_sw1_cleared.txt','w') as f2:
+    for line in f:
+        if ignore[0] in line:
+            pass
+        elif ignore[1] in line:
+            pass
+        elif ignore[2] in line:
+            pass
+        else:
+            f2.write(line)
