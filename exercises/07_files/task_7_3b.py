0531ec6e6f50a0587aa4d1810d34605053d98439
Egor-Ozhmegoff <banezmesm@gmail.com>
2020-12-12 23:50:11 +0300

    Сделано задание 7.3b

--- exercises/07_files/task_7_3b.py
+++ exercises/07_files/task_7_3b.py
@@ -11,3 +11,16 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+what_vlan = int(input('Введите номер vlan:'))
+
+mac_table = []
+
+with open('CAM_table.txt','r') as f:
+    for line in f:
+        line = line.split()
+        if line and line[0].isdigit():
+            vlan,mac,_,interface = line
+            mac_table.append([int(vlan),mac,interface])
+    for vlan,mac,interface in sorted(mac_table):
+        if what_vlan == vlan:
+            print("{:<4}  {:<14}  {:<6}".format(vlan,mac,interface))
