9701561f1050614c652aac59aba0edd41a2ba9c6
Egor-Ozhmegoff <banezmesm@gmail.com>
2021-03-08 15:53:06 +0300

    Сделано задание 12.1

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -9,8 +9,31 @@
 Функция должна возвращать кортеж с двумя списками:
 * список доступных IP-адресов
 * список недоступных IP-адресов
-
+ 
 Для проверки доступности IP-адреса, используйте команду ping.
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+import subprocess
+from sys import argv
+
+
+def ping_ip_addresses(ip_list):
+    reachable = []
+    unreachable = []
+    for ip in ip_list:
+        ping = subprocess.run(['ping', '-c', '4', '-n', ip], stdout=subprocess.DEVNULL)
+        if ping.returncode == 0:
+            reachable.append(ip)
+        else:
+            unreachable.append(ip)
+
+    result = (reachable, unreachable)
+    return result
+
+
+device = argv[1:]
+
+ip_status = ping_ip_addresses(device)
+print(ip_status)
