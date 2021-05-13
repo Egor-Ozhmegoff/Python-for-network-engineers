5f2b3317e16f8e02bdd265606a58dd0267571b04
Egor-Ozhmegoff <banezmesm@gmail.com>
2021-05-13 10:43:10 +0300

    Сделано задание 18.3

--- exercises/18_ssh_telnet/task_18_3.py
+++ exercises/18_ssh_telnet/task_18_3.py
@@ -32,5 +32,22 @@ Out[15]: 'config term\nEnter configuration commands, one per line.  End with CNT
 
 """
 
-commands = ["logging 10.255.255.1", "logging buffered 20010", "no logging console"]
-command = "sh ip int br"
+import yaml
+from task_18_1 import send_show_command
+from task_18_2 import send_config_commands
+
+
+def send_commands(device, show: str = None, config: list = None):
+    if show:
+        return send_show_command(device, show)
+    elif config:
+        return send_config_commands(device, config)
+
+
+if __name__ == "__main__":
+    commands = ["logging 10.255.255.1", "logging buffered 20010", "no logging console"]
+    command = "sh ip int br"
+    with open("devices.yaml") as f:
+        devices = yaml.safe_load(f)
+        for device in devices:
+            print(send_commands(device, config=commands))
