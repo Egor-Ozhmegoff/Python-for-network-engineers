2356b1b7d94f6b2b58727212e7224e52b5701399
Egor-Ozhmegoff <banezmesm@gmail.com>
2021-06-07 22:26:28 +0300

    Сделано задание 20.1

--- exercises/20_jinja2/task_20_1.py
+++ exercises/20_jinja2/task_20_1.py
@@ -14,11 +14,20 @@
 
 """
 import yaml
+from jinja2 import Environment, FileSystemLoader
+import os
+
+def generate_config(template, data_dict):
+    template_dir, template_parth = os.path.split(template)
+    env = Environment(loader = FileSystemLoader(template_dir))
+    template = env.get_template(template_parth)
+    result = template.render(data_dict)
+    return result
 
 
 # так должен выглядеть вызов функции
 if __name__ == "__main__":
-    data_file = "data_files/for.yml"
+    data_file = "data_files/for.yml"S
     template_file = "templates/for.txt"
     with open(data_file) as f:
         data = yaml.safe_load(f)
