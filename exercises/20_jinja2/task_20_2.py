7392bcb539526bdaa9a52c5eb9704af82622f09c
Egor-Ozhmegoff <banezmesm@gmail.com>
2021-06-08 10:56:59 +0300

    Сделано задание 20.2

--- exercises/20_jinja2/task_20_2.py
+++ exercises/20_jinja2/task_20_2.py
@@ -16,3 +16,14 @@
 В качестве данных, используйте информацию из файла data_files/router_info.yml
 
 """
+
+from task_20_1 import generate_config
+
+
+if __name__ == "__main__":
+    data_file = "data_files/router_info.yml"
+    template_file = "templates/cisco_router_base.txt"
+    with open(data_file) as f:
+        data = yaml.safe_load(f)
+    print(generate_config(template_file, data))
+
