b9e5e7a6c6cb9fdfcf8a2d09b285ddc0b38356fd
Egor-Ozhmegoff <banezmesm@gmail.com>
2021-09-09 14:52:28 +0300

    Сделано задание 22.1a

--- exercises/22_oop_basics/task_22_1a.py
+++ exercises/22_oop_basics/task_22_1a.py
@@ -9,12 +9,6 @@
 При этом метод __init__ должен выглядеть таким образом:
 """
 
-
-class Topology:
-    def __init__(self, topology_dict):
-        self.topology = self._normalize(topology_dict)
-
-
 topology_example = {
     ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
     ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
@@ -26,3 +20,20 @@ topology_example = {
     ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
     ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
 }
+
+class Topology:
+    def __init__(self, topology_dict):
+        self.topology = self._normalize(topology_dict)
+
+
+    def _normalize(self,topology_dict):
+        self.topology = {}
+        for local, remote in topology_dict.items():
+            if not self.topology.get(remote) == local:
+                self.topology[local] = remote
+        return self.topology
+
+
+if __name__ =='__main__':
+    top = Topology(topology_example)
+    print(top.topology)
